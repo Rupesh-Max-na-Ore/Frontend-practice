ffb2709b5fc7e1a80a53934515e8abd17af5b4cb
Rupesh <dasrupesh2124@gmail.com>
2025-06-19 00:53:19 +0530

    Removed plain js code comments

--- src/App.js
+++ src/App.js
@@ -8,15 +8,3 @@ const App = () => {
 }
 
 export default App;
-
-// // Above is same as this plain js code
-// import { jsx as _jsx } from "react/jsx-runtime";
-// const App = () => {
-//   return /*#__PURE__*/_jsx("div", {
-//     className: "App",
-//     children: /*#__PURE__*/_jsx("h1", {
-//       children: "Images Gallery"
-//     })
-//   });
-// };
-// export default App;
--- src/index.js
+++ src/index.js
@@ -10,14 +10,3 @@ root.render(
     <App />
   </React.StrictMode>
 );
-
-// // Above is same as this in plain js
-// import React from 'react';
-// import ReactDOM from 'react-dom/client';
-// import './css/index.css';
-// import App from './App';
-// import { jsx as _jsx } from "react/jsx-runtime";
-// const root = ReactDOM.createRoot(document.getElementById('root'));
-// root.render(/*#__PURE__*/_jsx(React.StrictMode, {
-//   children: /*#__PURE__*/_jsx(App, {})
-// }));
