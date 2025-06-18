94b22232e21f709a1ec6e62bfd44c8cb20c68b11
Rupesh <dasrupesh2124@gmail.com>
2025-06-19 00:51:34 +0530

    Arrow funcion and function equivalence, plain js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 
-function App() {
+const App = () => {
   return (
     <div className="App">
       <h1>Images Gallery</h1>
@@ -8,3 +8,15 @@ function App() {
 }
 
 export default App;
+
+// // Above is same as this plain js code
+// import { jsx as _jsx } from "react/jsx-runtime";
+// const App = () => {
+//   return /*#__PURE__*/_jsx("div", {
+//     className: "App",
+//     children: /*#__PURE__*/_jsx("h1", {
+//       children: "Images Gallery"
+//     })
+//   });
+// };
+// export default App;
