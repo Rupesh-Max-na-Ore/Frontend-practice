68abb655bdeeee862fbcacdec4e97c5265392f1c
Rupesh <dasrupesh2124@gmail.com>
2025-06-19 00:44:46 +0530

    Demo of how to do write js and html in react

--- src/index.js
+++ src/index.js
@@ -4,9 +4,22 @@ import './css/index.css';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+const a = true;
+
 root.render(
   <React.StrictMode>
-    <App />
+    {!a ? <App /> : <h1>Hello from JS in JSX</h1>}
   </React.StrictMode>
 );
 
+// // Above is same as this in plain js
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
+// import './css/index.css';
+// import App from './App';
+// import { jsx as _jsx } from "react/jsx-runtime";
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(/*#__PURE__*/_jsx(React.StrictMode, {
+//   children: /*#__PURE__*/_jsx(App, {})
+// }));
